a93b868a9edc2eff4e33d92264a03529c53c7791
1922635 <161758343+1922635@users.noreply.github.com>
2025-02-03 20:37:40 +0100

    Cambio View por ScrollView

--- app/drawer/marcha/index.tsx
+++ app/drawer/marcha/index.tsx
@@ -1,13 +1,15 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import Registro from '@/app/registro'
-import { GlobalStyles } from '@/theme/GlobalStyles'
-import { TextInput } from 'react-native-gesture-handler'
-import { Boton1 } from '@/components/boton1'
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import Registro from '@/app/registro';
+import { GlobalStyles } from '@/theme/GlobalStyles';
+import { TextInput } from 'react-native-gesture-handler';
+import { Boton1 } from '@/components/boton1';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ScreenProductos = () => {
   return (
-    <View style={GlobalStyles.registro}>
+    <ScrollView 
+      ContentContainerStyle={GlobalStyles.registro}>
 
       <View style={GlobalStyles.viewDarkBlue}>
         <Text style={GlobalStyles.textoAdoptar2}>Descubre eventos solidarios creados por la comunidad: actividades deportivas, caminatas y mucho más. Cada participación suma para ayudar a los animales que más lo necesitan. ¡Únete y se parte del cambio!.</Text>
@@ -35,7 +37,7 @@ const ScreenProductos = () => {
 
        <Boton1 label='+ Nueva' estilo1={GlobalStyles.botonDiscusion} estilo2={GlobalStyles.botonPulsadoDiscusion} link='./marcha'></Boton1>
 
-    </View>
+    </ScrollView>
   )
 }
 
